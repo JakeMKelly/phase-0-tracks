363f3510343b29c9d79b78f92d3b8aa78653890d
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-09-24 00:04:11 -0700

    Create user input and build hash with return of hash in hashes. Write practice hash in hash_practice.

---
+++ ruby/hash_practice.rb
@@ -0,0 +1,18 @@
+application = {
+	name: "Bob",
+	address: "2122 Sleepy Hollow Cir",
+	email: "bob_walks@gmail.com",
+	phone: 719-555-5534,
+	blue_shade: "Cerulean",
+	wallpaper: "Paisley, Abstract",
+	ombre: "So last season"	
+}
+
+p application
+application[:name] = "Robert"
+p application
+application[:age] = 25
+p application
+p application[:age]
+application[:name] + application[:address]
+p application
--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,8 +1,41 @@
-# Driver Code 
-# Create a method 
-
-
-
-
-
 # User Interface
+# Ask the designer for details for the client. Each response is a value.  
+client = {}
+
+puts "Client name?"
+client[:name] = gets.chomp
+
+puts "Preferred motif?"
+client[:theme] = gets.chomp
+
+puts "Number of rooms?"
+client[:rooms] = gets.to_i
+
+puts "Any pets? (y/n)"
+pets = gets.chomp
+if pets == "y"
+	pets_in_home = true
+else pets == "n"
+	pets_in_home = false
+end
+	
+client[:pets] = pets_in_home
+
+puts "Couch, futon, or pillowfort?"
+client[:furniture] = gets.chomp		
+
+# Driver Code 
+# Build an empty hash
+# Add each key/value pair to the new hash
+# Print the new hash for the user to read
+# Ask designer for a key to update
+	# IF the user says "none", no update
+	# ELSE ask the designer what the new value should be
+		# Replace the old value with the new value
+# Print the updated hash and exit the program
+puts "-" * 17
+puts "Here's your info:" 
+puts client
+puts "-" * 17
+
+

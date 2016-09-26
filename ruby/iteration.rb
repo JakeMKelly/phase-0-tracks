b38d886b17be6bd7f6ebf37057bf858644bc7dcd
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-09-26 00:45:59 -0700

    Add parameters to block

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,9 @@
 def practice
+	param1 = "first parameter"
+	param2 = "second parameter"
 	puts "This is practice."
-	yield
+	yield(param1, param2)
 end
 
-practice { puts "This is a block." }
+practice { |param1, param2| puts "This is a block. With #{param1} and #{param2}."
+ }

5995eef5a71f887e1581d0ab9122a90a450e8075
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-09-26 01:16:59 -0700

    Add more methods on structures

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,3 +7,32 @@ end
 
 practice { |param1, param2| puts "This is a block. With #{param1} and #{param2}."
  }
+
+cars = {:honda => "Accord", :chevy => "Corvette", :toyota => "Prius", :ford =>
+"Mustang"}
+
+prices = [500, 35, 276, 87, 9235, 863]
+
+p cars
+cars.each do |make, model|
+	puts "Did you know that #{make} builds the #{model}?" 
+end
+p cars
+p prices
+prices.map! do |price|
+	price + 1
+end
+p prices
+
+cars.delete_if {|make, model| make == :toyota }
+p cars
+prices.delete_if {|price| price < 100 }
+p prices
+cars.keep_if {|make, model| model.length >= 7 }
+p cars
+prices.keep_if {|price| price >= 501 }
+p prices
+cars.reject! {|make, model| model.length <= 7 }
+p cars
+prices.reject! {|price| price >= 8000 }
+p prices

81eb3fe8484440402a445f0cf5c334882b5794a9
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-10-01 12:05:30 -0700

    Comment out old code and re-write module as mixin, create two classes and include Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,26 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + ":("
-	end
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + ":("
+# 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + "XD XD XD"
-	end	
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + "XD XD XD"
+# 	end	
+# end
+
+# p Shout.yell_angrily("I can't find my brain today")
+# p Shout.yelling_happily("I found my brain")
+
+module Shout
+	def yell_louder(words)
+		words.upcase + "!!!!!"
+	end
 end
 
-p Shout.yell_angrily("I can't find my brain today")
-p Shout.yelling_happily("I found my brain")
+class Concert_goer
+	include Shout
+end
+
+class Ticket_scalper
+	include Shout
+end

fefe63fe395dd4a675023208f56ba4cc771c72ad
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-10-05 19:53:02 -0700

    Create loop for combining arrays into an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,9 @@ var names = ["Dave", "Bill", "Steve", "Charles"];
 colors.push("Purple");
 names.push("Josh");
 
+var stable = {};
+for (var i = 0; i < names.length; i++) {
+	stable[names[i]] = colors[i];
+};
+
+console.log(stable);

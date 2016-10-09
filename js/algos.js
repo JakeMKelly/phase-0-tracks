44f2edc03bcbeb5407bf7345b8f0c993a606742b
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-10-08 17:41:13 -0700

    Psuedocode release 0

---
+++ js/algos.js
@@ -0,0 +1,4 @@
+// Write function that returns the longest phrase or word in an array.
+// Use .length to compare the length of each.
+// Use an if statement to store the longest string in that variable as it iterates.
+// return the variable after iterating through entire array

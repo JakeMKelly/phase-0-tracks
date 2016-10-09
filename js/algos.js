2945704bd7f48a44cdc1851e47ed4147f4368312
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-10-08 21:42:44 -0700

    Create random string/array generator

--- js/algos.js
+++ js/algos.js
@@ -16,34 +16,52 @@ function length_finder(string_array) {
 // Release 2:
 // Write a function that iterates through an object and combines key/value into one string
 // if any string is equal to any string made from a second object, return true.
-function object_comparison(first_obj, second_obj){
-  var person_array = [];
-  var other_person_array = [];
-  match_bool = false;
-  for (var key in first_obj) {
-    person_array.push(key + first_obj[key]);
-  }
+// function object_comparison(first_obj, second_obj){
+//   var person_array = [];
+//   var other_person_array = [];
+//   match_bool = false;
+//   for (var key in first_obj) {
+//     person_array.push(key + first_obj[key]);
+//   }
 
-  for (var key2 in second_obj) {
-    other_person_array.push(key2 + second_obj[key2]);
-  }
+//   for (var key2 in second_obj) {
+//     other_person_array.push(key2 + second_obj[key2]);
+//   }
 
-  for (i = 0; i < person_array.length; i++) {
-      if (other_person_array.includes(person_array[i])) {
-        match_bool = true;
-      }
-  }
-  return match_bool;
+//   for (i = 0; i < person_array.length; i++) {
+//       if (other_person_array.includes(person_array[i])) {
+//         match_bool = true;
+//       }
+//   }
+//   return match_bool;
+// }
+
+// This works in repl.it, and ".includes" is covered on MDN. However, my version of node doesn't recognize .includes as a function
+// -----------------------------------------------
+// Write a function that builds an array made up of strings.  The length of the array is determined by the integer fed as an argument. 
+// The strings should be randomly chosen letters.
+// The length of each string should be a random number between 1 and 10
+function array_generator(length_integer){
+  var built_array = [];
+  var random_string = '';
+  var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  for (i = 0; i < length_integer; i++ ) {
+    for (v = 0; v <= Math.floor((Math.random() * 10) + 1); v++) {
+      random_string += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    }
+    built_array.push(random_string);
+    var random_string = '';
 }
-
-// This works in repl.it, and ".includes" is covered on MDN. However, my version of node doesn't recognize .includes as a function?
+return built_array;
+}
 
 // Driver Code
 var test_array = ["long phrase","longest phrase","longer phrase"];
 console.log (length_finder(test_array));
-var person = {name: "Steven", age: 54, gender: "male"};
-var other_person = {name: "Tamir", age: 54, gender: "undefined"};
-console.log (object_comparison(person, other_person));
-var person = {name: "Sally", age: 44, gender: "female"};
-var other_person = {name: "Tanya", age: 54, gender: "undefined"};
-console.log (object_comparison(person, other_person));
+// var person = {name: "Steven", age: 54, gender: "male"};
+// var other_person = {name: "Tamir", age: 54, gender: "undefined"};
+// console.log (object_comparison(person, other_person));
+// var person = {name: "Sally", age: 44, gender: "female"};
+// var other_person = {name: "Tanya", age: 54, gender: "undefined"};
+// console.log (object_comparison(person, other_person));
+console.log (array_generator(5));

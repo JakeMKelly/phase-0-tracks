d98094591c17aa7259c90fb45da164ec1281f790
Jacob M. Kelly <jakemkelly84@gmail.com>
2016-10-08 17:52:01 -0700

    Create comparison function

--- js/algos.js
+++ js/algos.js
@@ -2,3 +2,15 @@
 // Use .length to compare the length of each.
 // Use an if statement to store the longest string in that variable as it iterates.
 // return the variable after iterating through entire array
+// function search_array(array_to_search) {
+//   var longest_string = ''
+//   array_to_search.forEach(str.length)
+// }
+function longest_string(array_item) {
+  var longest_string = '';
+  if (array_item > longest_string) {
+    longest_string = array_item;
+  } else {
+    array_item = array_item;
+  }
+}
